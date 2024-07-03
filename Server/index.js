1c460be7c0e0ef9d4f4e67cbd4bbbb9f4ff62998
JEEVEEKA <jeeveeka.ps@kalvium.community>
2024-07-03 11:46:17 +0530

    added chat

--- Server/index.js
+++ Server/index.js
@@ -7,6 +7,8 @@ import AuthRoute from './Routes/AuthRoute.js'
 import UserRoute from './Routes/UserRoute.js'
 import PostRoute from './Routes/PostRoute.js'
 import UploadRoute from './Routes/UploadRoute.js'
+import ChatRoute from './Routes/ChatRoute.js'
+import MessageRoute from './Routes/MessageRoute.js'
 
 // Load environment variables from .env file
 dotenv.config();
@@ -17,6 +19,7 @@ const app = express();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors())
+
 // to serve images inside public folder
 app.use(express.static('public')); 
 app.use('/images', express.static('images'));
@@ -34,3 +37,5 @@ mongoose.connect(process.env.MONGO_DB, { useNewUrlParser: true, useUnifiedTopolo
   app.use('/user', UserRoute)
   app.use('/posts', PostRoute)
   app.use('/upload', UploadRoute)
+  app.use('/chat', ChatRoute)
+  app.use('/message', MessageRoute)
